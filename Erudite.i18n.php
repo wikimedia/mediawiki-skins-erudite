931d53b689ce0f9ca0f4c36bcb711b8b2ce39dd3
njw <nick.white@durham.ac.uk>
2013-05-20 18:00:40 +0100

    Add localisation documentation and update the description.
    I updated the description as it's gradually getting further away from Matt Wiebe's wordpress theme.
    
    Change-Id: I466e2cc29bdb2f66f32eb9a38f013710a36b7130

--- Erudite.i18n.php
+++ Erudite.i18n.php
@@ -12,17 +12,26 @@ $messages = array();
  * @author Nick White
  */
 $messages['en'] = array(
-	'skinname-erudite' => "Erudite",
-	'erudite-desc' => "Based on The Erudite theme for Wordpress by Matt Wiebe.",
-
+	'skinname-erudite' => 'Erudite',
+	'erudite-desc' => 'A clean and easy to read skin.',
 	'erudite-skiptocontent' => "Skip to content",
 	'erudite-skiptosearch' => "Skip to search",
-
-	// Tab text overrides
 	'erudite-action-addsection' => 'Add topic',
-
-	/* Optional extra footer content for columns (leave blank by default) */
 	'erudite-extracontent-column1' => "",
 	'erudite-extracontent-column2' => "",
 	'erudite-extracontent-column3' => "",
+);
+
+/** Message documentation
+ * @author Nick White
+ */
+$messages['qqq'] = array(
+	'skinname-erudite' => 'Skin name',
+	'erudite-desc' => '{{desc|name=Erudite|url=http://www.mediawiki.org/wiki/Skin:Erudite}}',
+	'erudite-skiptocontent' => "Link to skip directly to the content on the page",
+	'erudite-skiptosearch' => "Link to skip directly to the search box on the page",
+	'erudite-action-addsection' => "Link to add a new section to talk page, overriding the default of '+'",
+	'erudite-extracontent-column1' => "Extra content for footer column 1 (leave blank by default)",
+	'erudite-extracontent-column2' => "Extra content for footer column 2 (leave blank by default)",
+	'erudite-extracontent-column3' => "Extra content for footer column 3 (leave blank by default)",
 );
