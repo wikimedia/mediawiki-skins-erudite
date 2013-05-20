53dc117226c95777da1d8ea3f0aa2fcfe48f354d
njw <nick.white@durham.ac.uk>
2013-05-20 18:00:40 +0100

    Add localisation documentation and update the description.
    I updated the description as it's gradually getting further away from Matt Wiebe's wordpress theme.
    
    Change-Id: I466e2cc29bdb2f66f32eb9a38f013710a36b7130

--- Erudite.i18n.php
+++ Erudite.i18n.php
@@ -12,17 +12,26 @@ $messages = array();
  * @author Nick White
  */
 $messages['en'] = array(
-	'skinname-erudite' => "Erudite",
-	'erudite-desc' => "Based on The Erudite theme for Wordpress by Matt Wiebe.",
-
-	'erudite-skiptocontent' => "Skip to content",
-	'erudite-skiptosearch' => "Skip to search",
-
-	// Tab text overrides
+	'skinname-erudite' => 'Erudite',
+	'erudite-desc' => 'A clean and easy to read skin.',
+	'erudite-skiptocontent' => 'Skip to content',
+	'erudite-skiptosearch' => 'Skip to search',
 	'erudite-action-addsection' => 'Add topic',
 
-	/* Optional extra footer content for columns (leave blank by default) */
-	'erudite-extracontent-column1' => "",
-	'erudite-extracontent-column2' => "",
-	'erudite-extracontent-column3' => "",
+	/* Extra content for footer columns. Leave these empty here,
+	 * they are intended to be customised by site administrators. */
+	'erudite-extracontent-column1' => '', # do not translate or duplicate this message to other languages
+	'erudite-extracontent-column2' => '', # do not translate or duplicate this message to other languages
+	'erudite-extracontent-column3' => '', # do not translate or duplicate this message to other languages
+);
+
+/** Message documentation
+ * @author Nick White
+ */
+$messages['qqq'] = array(
+	'skinname-erudite' => 'Skin name',
+	'erudite-desc' => '{{desc|name=Erudite|url=http://www.mediawiki.org/wiki/Skin:Erudite}}',
+	'erudite-skiptocontent' => 'Link to skip directly to the content on the page',
+	'erudite-skiptosearch' => 'Link to skip directly to the search box on the page',
+	'erudite-action-addsection' => 'Link to add a new section to talk page, overriding the default of "+"',
 );
