81f8c431c3beeb70ba588d40394645c16bdedb9b
njw <nick.white@durham.ac.uk>
2013-07-18 12:46:40 +0100

    Release v1.2
    Change-Id: I465675e355a3eb62929a1c53a38f2a31404d548e

--- erudite.php
+++ erudite.php
@@ -17,7 +17,7 @@ $wgExtensionCredits['skin'][] = array(
 	'name' => 'Erudite',
 	'url' => 'https://www.mediawiki.org/wiki/Skin:Erudite',
 	'author' => array( 'Nick White', 'Matt Wiebe', 'Colin Andrew Ferm' ),
-	'version' => '1.1',
+	'version' => '1.2',
 	'descriptionmsg' => 'erudite-desc',
 );
 
