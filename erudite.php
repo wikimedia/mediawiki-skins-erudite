ccca94819747ca39c97936d1265a3c9ffbf00795
Kunal Mehta <legoktm@gmail.com>
2015-05-24 17:22:17 +0200

    Add skin.json, empty PHP entry point
    Change-Id: Ibd47611a291ce3886faa6513e60ec34711791cb9

--- erudite.php
+++ erudite.php
@@ -1,40 +1,13 @@
 <?php
-/**
- * Erudite MW skin
- *
- * @file
- * @ingroup Skins
- * @author Nick White
- * @author Matt Wiebe
- * @author Colin Andrew Ferm
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- */
-
-if( !defined( 'MEDIAWIKI' ) ) die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
-
-$wgExtensionCredits['skin'][] = array(
-	'path' => __FILE__,
-	'name' => 'Erudite',
-	'url' => 'https://www.mediawiki.org/wiki/Skin:Erudite',
-	'author' => array( 'Nick White', 'Matt Wiebe', 'Colin Andrew Ferm' ),
-	'version' => '1.8',
-	'descriptionmsg' => 'erudite-desc',
-);
-
-$wgValidSkinNames['erudite'] = 'Erudite';
-$wgAutoloadClasses['SkinErudite'] = __DIR__ . '/Erudite.skin.php';
-$wgMessagesDirs['Erudite'] = __DIR__ . '/i18n';
-$wgResourceModules['skins.erudite'] = array(
-	'styles' => array(
-		'assets/cssreset.css' => array( 'media' => 'screen' ),
-		'assets/erudite.css' => array( 'media' => 'screen' ),
-		'assets/erudite66em.css' => array( 'media' => 'screen and (max-width: 66em)' ),
-		'assets/erudite60em.css' => array( 'media' => 'screen and (max-width: 60em)' ),
-		'assets/erudite55em.css' => array( 'media' => 'screen and (max-width: 55em)' ),
-		'assets/erudite40em.css' => array( 'media' => 'screen and (max-width: 40em)' ),
-		'assets/erudite20em.css' => array( 'media' => 'screen and (max-width: 20em)' ),
-		'assets/print.css' => array( 'media' => 'print' ),
-	),
-	'remoteSkinPath' => 'erudite',
-	'localBasePath' => __DIR__,
-);
+if ( function_exists( 'wfLoadSkin' ) ) {
+	wfLoadSkin( 'erudite' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Erudite'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for erudite skin. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the erudite skin requires MediaWiki 1.25+' );
+}
